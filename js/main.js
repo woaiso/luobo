4c9398cc9ddc7343efdf536d6e0ee1cba49229a4
YunFu Liao <yunful@jumei.com>
2016-10-22 18:04:11 +0800

    add luobo

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".swiper-container").swiper({
+        speed: 400,
+        spaceBetween: 100,
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+})
